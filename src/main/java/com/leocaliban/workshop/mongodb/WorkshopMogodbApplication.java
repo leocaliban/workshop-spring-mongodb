178e0822e347d1b58e76ddf841096c476ea39870
Leocaliban <leocaliban@gmail.com>
2018-08-16 07:34:00 -0300

    Initial commit

---
+++ src/main/java/com/leocaliban/workshop/mongodb/WorkshopMogodbApplication.java
@@ -0,0 +1,12 @@
+package com.leocaliban.workshop.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMogodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMogodbApplication.class, args);
+	}
+}
